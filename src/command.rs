a90ef43b9bb413dbbb3b99c9abbc1c47a23d1288
ふかがわ <simonyen@foxmail.com>
2024-08-31 12:35:30 +0800

    受不了了，还是换成deepin,支持国产🇨🇳！

--- src/command.rs
+++ src/command.rs
@@ -2,23 +2,26 @@
 根据读取出来的配置文件，生成命令行语句
 */
 
-use ansi_rgb::{ red,green,Background };
-use std::process::Command;
 use crate::config::{Mode, Project};
+use ansi_rgb::{green, red, Background};
+use std::{fs, io, process::Command};
 
-struct OneLineCommand{
-    data:String,
+struct OneLineCommand {
+    data: String,
 }
 
 impl OneLineCommand {
-    fn new(data:String)->OneLineCommand{
-        return OneLineCommand{data:data};
+    fn new(data: String) -> OneLineCommand {
+        return OneLineCommand { data };
     }
     //执行命令
-    fn execute(& self){
-        let output = Command::new(&self.data)
-        .output()
-        .expect(format!("Failed to execute {}",&self.data).bg(red()).to_string().as_str());
+    fn execute(&self) {
+        let output = Command::new(&self.data).output().expect(
+            format!("Failed to execute {}", &self.data)
+                .bg(red())
+                .to_string()
+                .as_str(),
+        );
         // 将输出转换为字符串并打印
         let result = String::from_utf8_lossy(&output.stdout);
         println!("{}", result.bg(green()));
@@ -26,18 +29,56 @@ impl OneLineCommand {
 }
 
 //所有需要执行的命令
-pub struct AllCommand{
-    cmds:Vec<OneLineCommand>,
+pub struct AllCommand {
+    cmds: Vec<OneLineCommand>,
 }
 
 impl AllCommand {
-    pub fn new(project:&Project)->AllCommand{
+    pub fn new(project: &Project) -> AllCommand {
+        if !project.check_complier() {
+            panic!("C++ complier: {} is invaild!",project.complier.cxx);
+        }
+        if !project.check_ol() {
+            panic!("Opt {} is invaild!",project.complier.cxx);
+        }
+        if !project.check_std() {
+            panic!("{} is invaild!",project.complier.cxx);
+        }
         //这个构造函数实现很重要，需要慢慢写
-        let mut all_command=AllCommand{cmds:Vec::<OneLineCommand>::new()};
+        let mut all_command = AllCommand {
+            cmds: Vec::<OneLineCommand>::new(),
+        };
+        //获取当前路径
+        let mut current_path = std::env::current_dir().unwrap();
+        //获取所有源文件
+        let src_files = project.get_src_files();
+        current_path.push(".sm");
+        //0.创建.sm文件夹
+        match fs::metadata(current_path) {
+            Ok(metadata) => {
+                if metadata.is_file(){
+                    panic!("A file named .sm!");
+                }
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                // 目录不存在，创建它
+                fs::create_dir(current_path).unwrap();
+            },
+            Err(e) => {
+                // 处理其他错误
+                eprintln!("{}", e);
+            },
+        }
+        current_path.pop();
         match project.get_mode() {
             //编译为静态库时
-            Mode::Static=>{
-                
+            Mode::Static => {
+                //生成后置参数
+                let 
+                //1.将所有源文件编译成目标文件
+                for src_file in src_files{
+
+                }
             }
         }
         all_command
--- src/config.rs
+++ src/config.rs
@@ -2,35 +2,35 @@ use std::env;
 /*
 主要负责把project.toml里面的内容给读出来
 */
-use std::path::PathBuf;
-use std::fs::{read_dir, read_to_string};
 use serde::Deserialize;
+use std::fs::{read_dir, read_to_string};
+use std::path::PathBuf;
 
 #[derive(Deserialize)]
 pub struct Project {
-    pub target:Target,
-    pub complier:Complier,
+    pub target: Target,
+    pub complier: Complier,
 }
 
 #[derive(Deserialize)]
-pub struct Target{
-    pub name:String,
-    pub inc:String,
-    pub src:String,
-    pub entrance:String,
-    pub mode:String,
-    pub lib:String,
-    pub bin:String,
+pub struct Target {
+    pub name: String,
+    pub inc: String,
+    pub src: String,
+    pub entrance: String,
+    pub mode: String,
+    pub lib: String,
+    pub bin: String,
 }
 
 #[derive(Deserialize)]
-pub struct Complier{
-    pub cxx:String,
-    pub std:i64,
-    pub wall:bool,
-    pub ol:i8,
-    pub link:Vec<String>,
-    pub extra:Vec<String>,
+pub struct Complier {
+    pub cxx: String,
+    pub std: i64,
+    pub wall: bool,
+    pub ol: i8,
+    pub link: Vec<String>,
+    pub extra: Vec<String>,
 }
 
 //编译模式
@@ -40,75 +40,72 @@ pub enum Mode {
     Invalid,
 }
 
-
-
 impl Project {
-    pub fn new(config_path:&PathBuf)->Project{
+    pub fn new(config_path: &PathBuf) -> Project {
         //读取文件内容
-        let content=read_to_string(config_path).unwrap();
+        let content = read_to_string(config_path).unwrap();
         return toml::from_str(&content).unwrap();
     }
     //获取编译模式
-    pub fn get_mode(&self)->Mode{
+    pub fn get_mode(&self) -> Mode {
         match self.target.mode.as_str() {
-            "sta"=>{
-                Mode::Static
-            },
-            "dyn"=>{
-                Mode::Dynamic
-            },
-            _=>{
-                Mode::Invalid
-            },
+            "sta" => Mode::Static,
+            "dyn" => Mode::Dynamic,
+            _ => Mode::Invalid,
         }
     }
     //检查c++标准是否正确
-    pub fn check_std(&self)->bool{
+    pub fn check_std(&self) -> bool {
         match self.complier.std {
-            98|11|14|17|20=>{
-                true
-            },
-            _=>{false},
+            98 | 11 | 14 | 17 | 20 => true,
+            _ => false,
         }
     }
     //检查代码优化等级是否正确
-    pub fn check_ol(&self)->bool{
+    pub fn check_ol(&self) -> bool {
         match self.complier.ol {
-            0..=3=>{true},
-            _=>{false},
+            0..=3 => true,
+            _ => false,
+        }
+    }
+    //检查编译器是否设置正确
+    pub fn check_complier(&self) -> bool {
+        match self.complier.cxx.as_str() {
+            "g++" | "clang++" => true,
+            _ => false,
         }
     }
     //递归遍历函数
-    fn visit_dirs(&self,dir:&PathBuf,src_files:&mut Vec<PathBuf>){
+    fn visit_dirs(&self, dir: &PathBuf, src_files: &mut Vec<PathBuf>) {
         //递归退出条件
         if dir.is_file() {
             //收集c++源文件
             match dir.extension() {
-                Some(s)=>{
-                    if s=="cxx" || s=="cpp" || s=="hpp" {
+                Some(s) => {
+                    if s == "cxx" || s == "cpp" || s == "hpp" {
                         //添加到数组
                         src_files.push(dir.to_path_buf())
                     }
-                },
-                None=>{}
+                }
+                None => {}
             }
             return;
         }
         //递归遍历
-        for d in read_dir(dir).expect("Can't read dir!"){
+        for d in read_dir(dir).expect("Can't read dir!") {
             match d {
-                Ok(p)=>{
+                Ok(p) => {
                     self.visit_dirs(&p.path(), src_files);
-                },
-                _=>{}
+                }
+                _ => {}
             }
         }
     }
     //获取需要编译的源文件数组
-    pub fn get_src_files(&self)->Vec<PathBuf>{
-        let mut result:Vec<PathBuf>=Vec::new();
+    pub fn get_src_files(&self) -> Vec<PathBuf> {
+        let mut result: Vec<PathBuf> = Vec::new();
         //获取当前目录
-        let mut current_path=env::current_dir().unwrap();
+        let mut current_path = env::current_dir().unwrap();
         current_path.push(self.target.src.clone());
         self.visit_dirs(&current_path, &mut result);
         result
